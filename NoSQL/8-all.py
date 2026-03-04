389b1aea62b4b7c2df937748cbde8456c89fbfbb
SebaAlshedokhi <sebashedokhi@gmail.com>
2026-03-05 00:14:38 +0300

    Create 8-all.py

---
+++ NoSQL/8-all.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""List documents"""
+import pymongo
+
+
+def list_all(mongo_collection) -> list:
+    """
+    Lists all documents in a collection
+    """
+    documents: list = []
+
+    for document in mongo_collection.find():
+        documents.append(document)
+
+    return documents
