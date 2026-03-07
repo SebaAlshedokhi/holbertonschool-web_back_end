ebdac39fea07f581a03ffee28d58c7e9fae0ce66
SebaAlshedokhi <sebashedokhi@gmail.com>
2026-03-07 22:59:13 +0300

    Update 10-update_topics.py

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,1 +1,19 @@
+#!/usr/bin/env python3
+""" Update document """
+import pymongo
+from typing import List
 
+
+def update_topics(mongo_collection, name, topics):
+    """ Changes all topics of a school document based on the name
+
+        Args:
+            mongo_collection: collection to pass
+            name: school name to update
+            topics: list of topics
+    
+        Return:
+            Nothing
+    """
+    query: dict = {'name': name}
+    mongo_collection.update_many(query, {'$set': {'topics': topics}})
