819a61f6872c9b8f6c25ece2d95a7cf12ac32c89
SebaAlshedokhi <sebashedokhi@gmail.com>
2026-03-07 22:58:53 +0300

    Update 11-schools_by_topic.py

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,1 +1,16 @@
+#!/usr/bin/env python3
+""" Schools by topic """
+import pymongo
 
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Returns the list of school having a specific topic
+    """
+    query: dict = {'topics': topic}
+    schools: list = []
+
+    for school in mongo_collection.find(query):
+        schools.append(school)
+
+    return schools
