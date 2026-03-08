49c88d01e557e3343e2f64e2b2ad06c5ea0ad795
SebaAlshedokhi <sebashedokhi@gmail.com>
2026-03-08 23:12:25 +0300

    Update 12-log_stats.py

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    client = MongoClient()
+    client = MongoClient('mongodb://127.0.0.1:27017')
     db = client.logs
     collection = db.nginx
 
