e71ca679304412f2a37ec4a960cf6060d717f3c6
SebaAlshedokhi <sebashedokhi@gmail.com>
2026-04-11 15:23:23 +0300

    Add readDatabase function to read student data
    Implement readDatabase function to read and parse student data from a file.

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,1 +1,28 @@
+import fs from 'fs';
 
+const readDatabase = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf8', (error, fileContent) => {
+    if (error) {
+      reject(error);
+      return;
+    }
+
+    const lines = fileContent.split('\n').filter((line) => line.trim() !== '');
+    const students = lines.slice(1);
+    const studentsByField = {};
+
+    students.forEach((student) => {
+      const [firstname, , , field] = student.split(',');
+
+      if (!studentsByField[field]) {
+        studentsByField[field] = [];
+      }
+
+      studentsByField[field].push(firstname);
+    });
+
+    resolve(studentsByField);
+  });
+});
+
+export default readDatabase;
